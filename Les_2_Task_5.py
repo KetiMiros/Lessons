e54a48fb87dc329fbb5686ddacaef0d63adabe81
Ketrin_Miros <miroslavskayaketi@gmail.com>
2022-05-22 03:45:03 +0300

    Урок №2 Задание № 5

--- Les_2_Task_5.py
+++ Les_2_Task_5.py
@@ -9,26 +9,28 @@
 prices = [57.02, 45.56, 55, 20.17, 1.76, 51, 98.90, 70.01,
          65, 39, 90.48, 59.11, 88.08, 93, 8.53]
 
-print(f"\n\n{'*' * 35} часть_1\n")
-print(f"\n\n{'*' * 35} вариант_1\n")
+print(f"\n\n{'*' * 55} часть_1 {'*' * 55}\n")
+print(f"{'-' * 35} вариант_1\n")
 
 for i in prices:
     rub, kop = str(f"{i:.2f}").split(".")
     print(f"{rub} руб {int(kop):02d} коп ,", end=" ")
 
-print(f"\n\n{'*' * 35} вариант_2\n")
+print(f"\n\n{'-' * 35} вариант_2\n")
 
 for i in prices:
     rub, kop = int(i // 1), int(f"{i % 1:.02f}"[2:])
     print(f"{rub} руб {kop:02d} коп,", end=" ")
 
-print(f"\n\n{'*' * 35} часть_3\n")
+print(f"\n\n{'*' * 55} часть_3 {'*' * 55}\n")
+
 
 print(f"ID base: {id(prices)} - {prices}")
 prices.sort()
 print(f"ID change: {id(prices)} - {prices}")
 
-print(f"\n\n{'*' * 35} часть_4 и 5\n")
+print(f"\n\n{'*' * 55} часть_4 и 5 {'*' * 55}\n")
+
 
 n_list = sorted(prices, reverse=True)
 print(f"ID change: {id(n_list)} - {n_list}\n{n_list[:5]}")
