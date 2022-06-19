be42bdc4750df0f439271e09cb40f249cbd25ff3
Ketrin_Miros <miroslavskayaketi@gmail.com>
2022-06-19 04:36:23 +0300

    Урок №9 Задание № 5

---
+++ les_9_Task_5.py
@@ -0,0 +1,39 @@
+# Реализовать класс Stationery (канцелярская принадлежность).
+# определить в нём атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение «Запуск отрисовки»;
+# создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер);
+# в каждом классе реализовать переопределение метода draw. Для каждого класса метод должен выводить уникальное сообщение;
+# создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра
+
+class Stationery:
+    def __init__(self, title="Something that can draw"):
+        self.title = title
+
+    def draw(self):
+        print(f"Just start drawing! {self.title}🙂")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Start drawing with {self.title} pen!")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Start drawing with {self.title} pencil!")
+
+
+class Marker(Stationery):
+    def draw(self):
+        print(f"Start drawing with {self.title} marker!")
+
+
+stat = Stationery()
+pen = Pen("Parker")
+pencil = Pencil("Faber-Castell")
+marker = Marker("COPIK")
+
+office_suplies = [stat, pen, pencil, marker]
+
+for i in office_suplies:
+    i.draw()
+
